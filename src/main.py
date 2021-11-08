801185f8a1354ce275bf37b0d3412e9ea9563071
Kacper Amanowski <kacper3@yahoo.co.uk>
2021-11-08 23:11:50 +0000

    Cleaned up

--- src/main.py
+++ src/main.py
@@ -1,66 +1,19 @@
-import discord
-import json
-import random
 from discord.ext import commands
-import os
-import logging
-import datetime
-import asyncio
-
 from discord.ext.commands.bot import Bot
+import logging
+
 from command.PingCommand import PingCommand
 from command.RandCommand import RandCommand
+from util.ConfigReader import ConfigReader
 
 def initialiseCommands(bot: Bot):
     bot.add_cog(RandCommand(bot))
     bot.add_cog(PingCommand(bot))
-
+    
 logging.basicConfig(level=logging.INFO)
 
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, './config/json/auth.json')
-authF = open(filename,)
-TOKEN = json.load(authF)['token']
-
-
 bot = commands.Bot(command_prefix='£')
-bot.ctd_active = False
 
 initialiseCommands(bot)
-        
-async def ctd(ctx, bot):
-    await ctx.send('It be Sunday.')
-    while(bot.ctd_active):
-        now = datetime.datetime.now()
-        day = now.strftime("%A")
-        
-        #if day == 'Sunday':
-        await ctx.send('It be Sunday.')
-        
-        asyncio.sleep(1)
-        
-@bot.command(brief='Activates the Boys\' night countdown.', description='Does what it say on the tin.')
-async def commence(ctx, *args):
-    bot.ctd_active = True
-    await ctx.send('The countdown is now inactive.')
-    while(bot.ctd_active):
-        now = datetime.datetime.now()
-        day = now.strftime("%A")
-        
-        if day == 'Sunday':
-            await ctx.send('It be Sunday.')
-        
-        await asyncio.sleep(1)
-    #asyncio.run(ctd(ctx))
     
-    
-    
-    
-    
-@bot.command(brief='Deactivates the Boys\' night countdown.', description='Does what it say on the tin.')
-async def uncommence(ctx, *args):
-    bot.ctd_active = False
-    await ctx.send('The countdown is now inactive.')
-
-    
-bot.run(TOKEN)
+bot.run(ConfigReader.getToken())
--- src/util/ConfigReader.py
+++ src/util/ConfigReader.py
@@ -3,14 +3,16 @@ import json
 
 from exception.ConfigNotFound import ConfigNotFoundError
 
-def getToken() -> str:
-  dirname = os.path.dirname(__file__)
-  filename = os.path.join(dirname, './config/json/auth.json')
-  authF = open(filename,)
-  
-  token = json.load(authF)['token']
-  
-  if not token:
-    raise ConfigNotFoundError("Cannot find token.")
-  else:
-    return token
+class ConfigReader:
+
+  def getToken() -> str:
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../config/auth.json')
+    authF = open(filename,)
+    
+    token = json.load(authF)['token']
+    
+    if not token:
+      raise ConfigNotFoundError("Cannot find token.")
+    else:
+      return token
