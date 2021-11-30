ec8266dcb31980d7a64f2b71d42e515cf600a980
Kacper Amanowski <kacper3@yahoo.co.uk>
2021-11-30 21:20:05 +0000

    Kaiba updates Merlin's url

--- src/task/ServerCommandBlockTask.py
+++ src/task/ServerCommandBlockTask.py
@@ -1,22 +1,8 @@
-from typing import Text, Union
-from discord.abc import GuildChannel, PrivateChannel
-from discord.channel import TextChannel
 from discord.ext import commands, tasks
-from discord.ext.commands.bot import Bot
-from discord.ext.commands.context import Context
-from discord.message import Message
-from provider.Merlin import Merlin
-from exception.MerlinErrorException import MerlinErrorException
-
-
-from util.DynamicConfigReader import DynamicConfigReader
-from util.DynamicConfigWriter import DynamicConfigWriter
-
-import discord
 
 class ServerCommandBlockTask(commands.Cog):
     
-    MAX_COMMANDS = 5
+    MAX_COMMANDS = 500
     
     task_name = 'remove_server_command_blocks'
     
