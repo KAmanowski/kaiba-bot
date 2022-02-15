e74da457d58af430528cec3d5c3f05652da51b5c
Kacper Amanowski <kacper3@yahoo.co.uk>
2022-02-15 20:33:38 +0000

    Added sudo to commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
-pip install discord
-pip install discord-pretty-help
-pip install requests
-pip install pycron
-pip install rich
+sudo pip install discord
+sudo pip install discord-pretty-help
+sudo pip install requests
+sudo pip install pycron
+sudo pip install rich
